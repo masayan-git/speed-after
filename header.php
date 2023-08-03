6927976a60beff3032d616b973e6619d63034203
masayan-git <68589347+masayan-git@users.noreply.github.com>
2023-08-03 23:06:29 +0900

    font displayを追加

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0" />
   <meta name="format-detection" content="telephone=no" />
   <!-- meta情報 -->
-  <title>speed-before</title>
+  <title>speed-after</title>
   <meta name="description" content="" />
   <meta name="keywords" content="なし" />
   <meta name="robots" content="noindex">
